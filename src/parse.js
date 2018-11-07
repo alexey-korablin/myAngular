dbe458660ae1f6c4cd5c130b9df57d2d68b90573
Alexey Korablin <Alexey_Korablin@epam.com>
2018-11-07 16:31:54 +0300

    062_angular Parsing integers

--- src/parse.js
+++ src/parse.js
@@ -9,7 +9,42 @@ function parse(expr) {
 class Lexer {
 
     lex(text) {
+        this.text = text;
+        this.index = 0;
+        this.ch = null;
+        this.tokens = [];
 
+        while (this.index < this.text.length) {
+            this.ch = this.text.charAt(this.index);
+            if (this.isNumber(this.ch)) {
+                this.readNumber();
+            } else {
+                throw `Unexpected next chracter ${this.ch}`;
+            }
+        }
+
+        return this.tokens;
+    }
+
+    isNumber(ch) {
+        return '0' <= ch && ch <= '9';
+    }
+
+    readNumber() {
+        let number = '';
+        while (this.index < this.text.length) {
+            const ch = this.text.charAt(this.index);
+            if (this.isNumber(ch)) {
+                number += ch;
+            } else {
+                break;
+            }
+            this.index++;
+        }
+        this.tokens.push({
+            text: number,
+            value: Number(number)
+        });
     }
 }
 
@@ -20,6 +55,23 @@ class AST {
 
     ast(text) {
         this.tokens = this.lexer.lex(text);
+        return this.program();
+    }
+
+    program() {
+        return { type: AST.Program, body: this.constant() };
+    }
+
+    constant() {
+        return { type: AST.Literal, value: this.tokens[0].value };
+    }
+
+    static get Program() {
+        return 'Program';
+    }
+
+    static get Literal() {
+        return 'Literal';
     }
 }
 
@@ -30,6 +82,20 @@ class ASTCompiller {
 
     compile(text) {
         const ast = this.astBuilder.ast(text);
+        this.state = { body: [] };
+        this.recurse(ast);
+        return new Function(this.state.body.join(' '));
+    }
+
+    recurse(ast) {
+        switch (ast.type) {
+            case AST.Program:
+                this.state.body.push('return', this.recurse(ast.body), ';');
+                break;
+
+            case AST.Literal:
+                return ast.value;
+        }
     }
 }
 
@@ -45,4 +111,4 @@ class Parser {
     }
 }
 
-module.exports = parse;
+module.exports = parse; 
