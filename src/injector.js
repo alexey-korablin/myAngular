d268c47d3c0e66679cf0a09c7feecd4bbd61aac9
Alexey Korablin <Alexey_Korablin@epam.com>
2019-03-19 17:08:12 +0300

    06_angular_DI Requiring other modules

--- src/injector.js
+++ src/injector.js
@@ -4,19 +4,24 @@ const _ = require('lodash');
 
 function createInjector(modulesToLoad) {
     const cache = {};
+    const loadedModules = {};
     const $provide = {
         constant: function (key, value) {
             if ( key === 'hasOwnProperty' ) { throw 'hasOwnProperty is not valid constant name!'; }
             cache[key] = value;
         }
     };
-    _.forEach(modulesToLoad, (moduleName) => {
-        const module = window.angular.module(moduleName);
-        _.forEach(module._invokeQueue, (invokeArgs) => {
-            const method = invokeArgs[0];
-            const args = invokeArgs[1];
-            $provide[method].apply($provide, args);
-        });
+    _.forEach(modulesToLoad, function loadModule(moduleName) {
+        if (!loadedModules.hasOwnProperty(moduleName)) {
+            loadedModules[moduleName] = true;
+            const module = window.angular.module(moduleName);
+            _.forEach(module.requires, loadModule);
+            _.forEach(module._invokeQueue, (invokeArgs) => {
+                const method = invokeArgs[0];
+                const args = invokeArgs[1];
+                $provide[method].apply($provide, args);
+            });
+        }
     });
     return { 
         has: function (key) { return cache.hasOwnProperty(key); },
